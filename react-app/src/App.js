cc85ed58759db5f8732713a2fa0e3bc65d233b8f
HannahAmaria <113430366+HannahAmaria@users.noreply.github.com>
2024-07-31 22:19:14 +0200

    frontend for login signup and authentication done

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+//import './App.css';
+import LoginPage from './Pages/login/login.jsx';
+import SignUp from './Pages/createAccount/createAccount.jsx';
+import Authentication from './Pages/Authentication/authentication.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<LoginPage />} ></Route>
+        <Route path='/SignUp' element={<SignUp />} ></Route>
+        <Route path='/Authentication' element={<Authentication />} ></Route>
+      </Routes>
+    </Router>
   );
 }
 
