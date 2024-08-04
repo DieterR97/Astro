c40ccb6fe3ca05c5e027f030dcbaf168e2a8bb26
DupieM <221326@virtualwindow.co.za>
2024-08-04 18:53:20 +0200

    Final Frontend design for the admin Portal

--- react-app/src/Pages/Admin_Portal/Admin.tsx
+++ react-app/src/Pages/Admin_Portal/Admin.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import styles from './Admin.module.scss'
+import img from '../Assets/Filter_Option.png';
+import img2 from '../Assets/Search_Option.png';
+import img3 from '../Assets/Purple_Circle.png';
+import img4 from '../Assets/More_Option.png';
 
 function Admin() {
     return (
@@ -9,8 +13,14 @@ function Admin() {
                 <h2 className={styles.sub_head}>User</h2>
 
                 <div className={styles.admin_container_two}>
-                    <h4 className={styles.body}>Filter</h4>
-                    <h4 className={styles.body_two}>Search</h4>
+                    <h4 className={styles.body}>
+                        <img src={img} alt="Logo" className={styles.image}/>
+                        Filter
+                    </h4>
+                    <h4 className={styles.body_two}>
+                        <img src={img2} alt="Logo" className={styles.image}/>
+                        Search
+                    </h4>
                 </div>
                 
                 <div className={styles.admin_card}>
@@ -22,6 +32,41 @@ function Admin() {
                         <p className={styles.paragraph_five}>Action</p>
                     </div>
 
+                    <div className={styles.admin_card_two}>
+                        <img src={img3} alt="Logo" className={styles.image2}/>
+                        <div className={styles.name_box}>
+                            <p className={styles.paragraph_six}>Name Surname</p>
+                            <p className={styles.paragraph_seven}>302442</p>
+                        </div>
+                        <div className={styles.join_date_box}>
+                            <p className={styles.paragraph_six}>11 July 2024</p>
+                            <p className={styles.paragraph_seven}>At 11:00 PM</p>
+                        </div>
+                        <p className={styles.paragraph_eight}>32142</p>
+                        <p className={styles.paragraph_nine}>Active</p>
+                        <p className={styles.body_three}>
+                            Details
+                            <img  src={img4} alt="Logo" className={styles.image3}/>
+                        </p>
+                    </div>
+
+                    <div className={styles.admin_card_two}>
+                        <img src={img3} alt="Logo" className={styles.image2}/>
+                        <div className={styles.name_box}>
+                            <p className={styles.paragraph_six}>Name Surname</p>
+                            <p className={styles.paragraph_seven}>302442</p>
+                        </div>
+                        <div className={styles.join_date_box}>
+                            <p className={styles.paragraph_six}>11 July 2024</p>
+                            <p className={styles.paragraph_seven}>At 11:00 PM</p>
+                        </div>
+                        <p className={styles.paragraph_eleven}>32142</p>
+                        <p className={styles.paragraph_ten}>Inactive</p>
+                        <p className={styles.body_four}>
+                            Details
+                            <img  src={img4} alt="Logo" className={styles.image3}/>
+                        </p>
+                    </div>
 
                 </div>
             </div> 
