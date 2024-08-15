fcabcd77cb81828c6cfce389c996aca7f490fbe4
Erik <113360820+EConradie@users.noreply.github.com>
2024-08-15 08:04:46 +0200

    Fixed navbar issue. Now displays within pages and not authentication pages..

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,42 +1,54 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
-import Admin from './Pages/Admin_Portal/Admin';
-import Authentication from './Pages/Authentication/Authentication';
-import CreateAccount from './Pages/createAccount/createAccount';
-import Login from './Pages/login/login';
-import Overview from './Pages/Overview/Overview';
-import Purchasing from './Pages/Purchasing_Currency/Purchasing';
-import Transactions from './Pages/Transactions/Transactions';
-import Withdrawals from './Pages/Withdrawals/Withdrawals';
-import Navbar from './Components/Navbar/Navbar';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import Admin from "./Pages/Admin_Portal/Admin";
+import Authentication from "./Pages/Authentication/Authentication";
+import CreateAccount from "./Pages/createAccount/createAccount";
+import Login from "./Pages/login/login";
+import Overview from "./Pages/Overview/Overview";
+import Purchasing from "./Pages/Purchasing_Currency/Purchasing";
+import Transactions from "./Pages/Transactions/Transactions";
+import Withdrawals from "./Pages/Withdrawals/Withdrawals";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <Router>
-      <div style={{ display: 'flex' }}>
-        <NavRoutes />
-      </div>
+      <NavRoutes />
     </Router>
   );
 }
 
 function NavRoutes() {
   const location = useLocation();
-  const showNavbar = location.pathname !== "/";
+  const noNavbarRoutes = ["/login", "/authentication", "/"];
+
+  const showNavbar = !noNavbarRoutes.includes(location.pathname);
+
+  const contentStyle = showNavbar
+    ? { flex: 1, marginLeft: "264px" }
+    : {
+        height: "100vh",
+        width: "100vw",
+      };
 
   return (
     <>
-      {/* {showNavbar && <Navbar />} */}
-      <div style={{ flex: 1, marginLeft: '264px' }}>
+      {showNavbar && <Navbar />}
+      <div style={contentStyle}>
         <Routes>
-          <Route path="/Login" element={<Login />}></Route>
-          <Route path="/Authentication" element={<Authentication />}></Route>
-          <Route path="/" element={<CreateAccount />}></Route>
-          <Route path="/admin" element={<Admin />}></Route>
-          <Route path="/overview" element={<Overview />}></Route>
-          <Route path="/purchasing" element={<Purchasing />}></Route>
-          <Route path="/transactions" element={<Transactions />}></Route>
-          <Route path="/withdrawals" element={<Withdrawals />}></Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/authentication" element={<Authentication />} />
+          <Route path="/" element={<CreateAccount />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/overview" element={<Overview />} />
+          <Route path="/purchasing" element={<Purchasing />} />
+          <Route path="/transactions" element={<Transactions />} />
+          <Route path="/withdrawals" element={<Withdrawals />} />
         </Routes>
       </div>
     </>
