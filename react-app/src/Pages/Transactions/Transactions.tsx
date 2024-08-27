9463fd0b6612d739c9eb33a4b06d84ca12004fd8
Ungerer221 <221302@virtualwindow.co.za>
2024-08-27 12:25:32 +0200

    small changes

--- react-app/src/Pages/Transactions/Transactions.tsx
+++ react-app/src/Pages/Transactions/Transactions.tsx
@@ -70,22 +70,7 @@ function Transactions() {
     };
 
 
-    // * Details button Modal .....................................................................
-    // TODO : the details button
-    // const [isModalOpen, setIsModalOpen] = useState(false);
-    // const openModal = () => {
-    //     setIsModalOpen(true);
-    // };
 
-    // const closeModal = () => {
-    //     setIsModalOpen(false);
-    // };
-    // const openDetailsModal = (event: React.MouseEvent<HTMLDivElement>) => {
-    //     // Close the modal if the user clicks on the overlay (outside the modal content)
-    //     if (event.target === event.currentTarget) {
-    //         closeModal();
-    //     }
-    // };
 
 
     // * GET all the users transactions ...........................................................
@@ -109,20 +94,75 @@ function Transactions() {
     // console.log(userTransactions);
 
 
+    // * FILTERING FUNCTOINALITY ....................................................................
+
+    // TODO : filtering function for all - 7days - 24Hours 
+    // const filterRecentData = (items: Transaction[]): Transaction[] => {
+    //     const now = new Date();
+
+    //     const thirtyDaysAgo = new Date(now.setDate(now.getDate() - 30)); // 30 days
+    //     const sevenDaysAgo = new Date(now.setDate(now.getDate() - 7)); // 7 days
+    //     // Filter transactions from the past 24 hours
+    //     const twentyFourHoursAgo = new Date(now.setDate(now.getDate() - 24 * 60 * 60 * 1000)); // 24 hours
+
+    //     return items.filter(item => {
+    //         const itemDate = new Date(item.timestamp);
+    //         return itemDate >= thirtyDaysAgo;
+    //     });
+    // };
+    // * all Filter:
+    function allFilter() {
+        alert("all items filter");
+    };
+
+    // * Thirty days ago filter:
+    function thrityDaysFilter() {
+        const nowThrity = new Date();
+        const thirtyDaysAgo = new Date(nowThrity.setDate(nowThrity.getDate() - 30)); // 30 days
+        // console.log("THIRTY DAYS AGO:", thirtyDaysAgo); //  returns the date from 30 days ago
+        const thirtyDaysAgoData = incomingData.filter((a) => a.timestamp > thirtyDaysAgo);
+        // console.log(thirtyDaysAgoData);
+
+        alert("30 days filter button function was triggered ");
+
+    };
+
+    // * Seven Days ago filter:
+    function sevenDaysFilter() {
+        const nowSeven = new Date();
+        // console.log("Current date:",nowSeven);
+        const sevenDaysAgo = new Date(nowSeven.setDate(nowSeven.getDate() - 7)); // 7 days
+        // console.log("DATE 7 DYAS AGO:", sevenDaysAgo);
+        const sevenDaysAgoData = incomingData.filter((a) => a.timestamp > sevenDaysAgo);
+        // console.log(sevenDaysAgoData);
+        const sortedData = defualtSortingByDate(incomingData);
+
+        alert("7 days filter button function was triggered ");
+
+    };
+
+    // * 24 hours filter option:
+    function twentyFourHoursFilter() {
+        alert("within 24 hours filter button function was triggered ");
+    }
+
+
     // * SORTING FUNCTOINALITY ....................................................................
-    // TODO : by DEFUALT must be sorted by date
+    // hove the option set a value and sort triggered with the value
+    // * Defualt sorting (by timestamp):
     function defualtSortingByDate(data: Transaction[]): Transaction[] {
         return data.sort((a, b) => b.timestamp.getTime() - a.timestamp.getTime());
     }
 
+    // * Sorting by the Amount:
     // ? how to execute this...
     function sortingByAmount(data: Transaction[]): Transaction[] {
         return data.sort((a, b) => b.amount - a.amount); // sorting by the amount (decending = biggest to smallest)
     }
 
-    // Process and sort the data
+    // * this the what results in the sorting of data that is rendered 
     const sortedData = defualtSortingByDate(incomingData);
-    // const resultDataOfAmount = sortingByAmount(incomingData);   
+    // const resultDataOfAmount = sortingByAmount(incomingData);
     // console.log(sortedData);
 
 
@@ -145,89 +185,6 @@ function Transactions() {
     };
 
 
-    // * FILTERING FUNCTOINALITY ....................................................................
-    // TODO : filtering function for all - 7days - 24Hours 
-    // const filterRecentData = (items: Transaction[]): Transaction[] => {
-    //     const now = new Date();
-
-    //     const thirtyDaysAgo = new Date(now.setDate(now.getDate() - 30)); // 30 days
-    //     const sevenDaysAgo = new Date(now.setDate(now.getDate() - 7)); // 7 days
-    //     // Filter transactions from the past 24 hours
-    //     const twentyFourHoursAgo = new Date(now.setDate(now.getDate() - 24 * 60 * 60 * 1000)); // 24 hours
-
-    //     return items.filter(item => {
-    //         const itemDate = new Date(item.timestamp);
-    //         return itemDate >= thirtyDaysAgo;
-    //     });
-    // };
-
-    // * Thirty days ago filter:
-    function thrityDaysFilter() {
-        const nowThrity = new Date();
-        const thirtyDaysAgo = new Date(nowThrity.setDate(nowThrity.getDate() - 30)); // 30 days
-        // console.log("THIRTY DAYS AGO:", thirtyDaysAgo); //  returns the date from 30 days ago
-        const thirtyDaysAgoData = incomingData.filter((a) => a.timestamp > thirtyDaysAgo);
-        // console.log(thirtyDaysAgoData);
-    };
-
-    // * Seven Days ago filter:
-    function sevenDaysFilter() {
-        const nowSeven = new Date();
-        // console.log("Current date:",nowSeven);
-        const sevenDaysAgo = new Date(nowSeven.setDate(nowSeven.getDate() - 7)); // 7 days
-        // console.log("DATE 7 DYAS AGO:", sevenDaysAgo);
-        const sevenDaysAgoData = incomingData.filter((a) => a.timestamp > sevenDaysAgo);
-        // console.log(sevenDaysAgoData);
-        const sortedData = defualtSortingByDate(incomingData);
-        {incomingData && incomingData.length > 0 ? (
-                            incomingData.map(transaction => (
-                                <div className={styles.contentRowTile}>
-                                    {/* name data block */}
-                                    <div className={styles.nameDataBlock}>
-                                        <div className={styles.userProfilePlacholder}></div>
-                                        {/* User name data and number data block */}
-                                        <div className={styles.userNameData}>
-                                            <p className={styles.tableText01}>{transaction.userName}</p>
-                                            <p className={styles.dataIdNumber}>{transaction.transactions_id}</p>
-                                        </div>
-                                    </div>
-                                    {/* Date data block */}
-                                    <div className={styles.dateDataBlock}>
-                                        <div className={styles.transactionDateBlock}>
-                                            <p className={styles.tableText01}>{transaction.timestamp.getDate()} :</p>
-                                            <p className={styles.tableText01}>{transaction.timestamp.getMonth()} :</p>
-                                            <p className={styles.tableText01}>{transaction.timestamp.getFullYear()}</p>
-                                        </div>
-                                        <div className={styles.timeDataBlock}>
-                                            <p className={styles.timeData}>At {transaction.timestamp.getHours()} : </p>
-                                            <p className={styles.timeData}>{transaction.timestamp.getMinutes()}</p>
-                                        </div>
-                                    </div>
-                                    {/* Amount Data Block */}
-                                    <div className={styles.amountDataBlock}>
-                                        <p className={styles.tableText01}>R{transaction.amount}</p>
-                                    </div>
-                                    {/* invoice Id Data Block */}
-                                    <div className={styles.invoiceDataBlock}>
-                                        <p>INV34598</p>
-                                    </div>
-                                    {/* states data block */}
-                                    <div className={styles.statusDataBlock}>
-                                        <p className={styles.positiveText}>{transaction.transaction_type}</p>
-                                    </div>
-                                    {/* Action data block */}
-                                    <div className={styles.actionDataBlock}>
-                                        <button className={styles.detailsBtn}>Details
-                                            <img src={VerticalDots} alt="vertical" />
-                                        </button>
-                                    </div>
-                                </div>
-                            ))) : (
-                            <p>No transactions Found</p>
-                        )}
-    };
-
-
     // * OTHER ....................................................................................
     const monthNames = [
         'January', 'February', 'March', 'April', 'May', 'June',
@@ -235,6 +192,23 @@ function Transactions() {
     ];
     // Get the current month index (0-11)
     // const currentMonthIndex = currentDate.getMonth();
+
+    // * Details button Modal .....................................................................
+    // TODO : the details button
+    // const [isModalOpen, setIsModalOpen] = useState(false);
+    // const openModal = () => {
+    //     setIsModalOpen(true);
+    // };
+
+    // const closeModal = () => {
+    //     setIsModalOpen(false);
+    // };
+    // const openDetailsModal = (event: React.MouseEvent<HTMLDivElement>) => {
+    //     // Close the modal if the user clicks on the overlay (outside the modal content)
+    //     if (event.target === event.currentTarget) {
+    //         closeModal();
+    //     }
+    // };
 
     // ...
 
@@ -248,16 +222,24 @@ function Transactions() {
                     {/* filter bar */}
                     <div className={styles.filterBar}>
                         <div className={styles.filterOption01}>
-                            <p className={styles.filterOptionText}>All</p>
+                            <button id={styles.allFilterBtn} onClick={allFilter}>
+                                <p className={styles.filterOptionText}>All</p>
+                            </button>
                         </div>
                         <div className={styles.filterOption}>
-                            <p className={styles.filterOptionText} onClick={thrityDaysFilter}>30 Days</p>
+                            <button id={styles.thrityDaysFilterBtn} onClick={thrityDaysFilter}>
+                                <p className={styles.filterOptionText} >30 Days</p>
+                            </button>
                         </div>
-                        <div className={styles.filterOption}>
-                            <p className={styles.filterOptionText} onClick={sevenDaysFilter}>7 Days</p>
+                        <div className={styles.filterOption} >
+                            <button id={styles.sevenDaysFilterBtn} onClick={sevenDaysFilter}>
+                                <p className={styles.filterOptionText} >7 Days</p>
+                            </button>
                         </div>
                         <div className={styles.filterOption04}>
-                            <p className={styles.filterOptionText}>24 Hours</p>
+                            <button id={styles.twentyFourHoursFilterBtn} onClick={twentyFourHoursFilter}>
+                                <p className={styles.filterOptionText}>24 Hours</p>
+                            </button>
                         </div>
                     </div>
                     {/* other options group */}
@@ -269,11 +251,11 @@ function Transactions() {
                                 Filter
                             </button>
                             <div className={styles.dropDownContent}>
-                                <button>By Date </button>
-                                <button>By Name</button>
-                                <button>By Amount</button>
-                                <button>By Invoice</button>
-                                <button>By Status</button>
+                                <button value={"timestamp"}>By Date </button> 
+                                <button value={"byName"}>By Name</button>
+                                <button value={"amount"}>By Amount</button>
+                                <button value={""}>By Invoice</button>
+                                <button value={"byStatus"}>By Status</button>
                             </div>
                         </div>
                         {/* search field */}
@@ -379,10 +361,9 @@ function Transactions() {
                             ))) : (
                             <p>No transactions Found</p>
                         )}
-
                         <h3>Testing Dummy data below:</h3>
                         <p>Defualt Sorting is by Date </p>
-                        {/* mapping the data */}
+                        {/* // * mapping the Dummy data */}
                         {incomingData && incomingData.length > 0 ? (
                             incomingData.map(transaction => (
                                 <div className={styles.contentRowTile}>
