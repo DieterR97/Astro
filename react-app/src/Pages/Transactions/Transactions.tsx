462c5192f3e12e224f0249b187db11cc5c60d978
Ungerer221 <221302@virtualwindow.co.za>
2024-08-27 17:55:22 +0200

    Sorting & Filtering combo function done

--- react-app/src/Pages/Transactions/Transactions.tsx
+++ react-app/src/Pages/Transactions/Transactions.tsx
@@ -12,6 +12,7 @@ import { useParams } from 'react-router-dom';
 // ...
 
 type FilterOption = 'ALL' | 'LAST_7_DAYS' | 'LAST_30_DAYS' | 'LAST_24_HOURS';
+type SortOption = 'TIMESTAMP' | 'AMOUNT' | 'TRANSACTION_ID';
 
 function Transactions() {
 
@@ -56,22 +57,22 @@ function Transactions() {
 
     // * DUMMY DATA ...............................................................................
     const incomingData: Transaction[] = [
-        { transactions_id: 1, userName: 'Ungerer Hattingh', from_account_id: 4, transaction_type: "big", amount: 2500, timestamp: new Date('2024-06-12T12:00:00Z') },
-        { transactions_id: 1, userName: 'Ungerer Hattingh', from_account_id: 4, transaction_type: "big", amount: 2500, timestamp: new Date('2024-06-13T12:00:00Z') },
-        { transactions_id: 1, userName: 'Ungerer Hattingh', from_account_id: 4, transaction_type: "big", amount: 2500, timestamp: new Date('2024-06-27T12:00:00Z') },
-        { transactions_id: 2, userName: 'Ungerer Hattingh', from_account_id: 4, transaction_type: "big", amount: 2500, timestamp: new Date('2024-07-17T12:00:00Z') },
-        { transactions_id: 2, userName: 'Ungerer Hattingh', from_account_id: 4, transaction_type: "big", amount: 2500, timestamp: new Date('2024-07-18T12:00:00Z') },
-        { transactions_id: 2, userName: 'Ungerer Hattingh', from_account_id: 4, transaction_type: "big", amount: 2500, timestamp: new Date('2024-07-15T12:00:00Z') },
-        { transactions_id: 3, userName: 'Ungerer Hattingh', from_account_id: 4, transaction_type: "small", amount: 3000, timestamp: new Date('2024-08-01T09:00:00Z') },
-        { transactions_id: 4, userName: 'Test for date sorting', from_account_id: 4, transaction_type: "medium", amount: 500, timestamp: new Date('2024-08-21T15:00:00Z') },
-        { transactions_id: 5, userName: 'Ungerer Hattingh', from_account_id: 4, transaction_type: "medium", amount: 5000000, timestamp: new Date('2024-08-22T15:00:00Z') },
-        { transactions_id: 6, userName: 'Ungerer Hattingh', from_account_id: 4, transaction_type: "medium", amount: 54500, timestamp: new Date('2024-08-23T15:00:00Z') },
-        { transactions_id: 7, userName: 'Ungerer Hattingh', from_account_id: 4, transaction_type: "medium", amount: 67000, timestamp: new Date('2024-08-24T15:00:00Z') },
-        { transactions_id: 8, userName: 'Test for date sorting', from_account_id: 4, transaction_type: "medium", amount: 500, timestamp: new Date('2024-08-25T16:40:00Z') },
-        { transactions_id: 9, userName: 'Test for date sorting', from_account_id: 4, transaction_type: "medium", amount: 500, timestamp: new Date('2024-08-25T16:45:00Z') },
-        { transactions_id: 10, userName: 'Test for date sorting', from_account_id: 4, transaction_type: "medium", amount: 500, timestamp: new Date('2024-08-25T16:45:00Z') },
-        { transactions_id: 11, userName: 'Test for date sorting', from_account_id: 4, transaction_type: "medium", amount: 500, timestamp: new Date('2024-08-27T16:45:00Z') },
-        { transactions_id: 12, userName: 'Test for date sorting', from_account_id: 4, transaction_type: "medium", amount: 500, timestamp: new Date('2024-08-26T14:45:00Z') },
+        { transactions_id: 1, userName: 'Ungerer Hattingh', from_account_id: 2, transaction_type: "big", amount: 2500, timestamp: new Date('2024-06-12T12:00:00Z') },
+        { transactions_id: 2, userName: 'Ungerer Hattingh', from_account_id: 2, transaction_type: "big", amount: 2500, timestamp: new Date('2024-06-13T12:00:00Z') },
+        { transactions_id: 3, userName: 'Ungerer Hattingh', from_account_id: 2, transaction_type: "big", amount: 2500, timestamp: new Date('2024-06-27T12:00:00Z') },
+        { transactions_id: 4, userName: 'Ungerer Hattingh', from_account_id: 2, transaction_type: "big", amount: 2500, timestamp: new Date('2024-07-17T12:00:00Z') },
+        { transactions_id: 5, userName: 'Ungerer Hattingh', from_account_id: 2, transaction_type: "big", amount: 2500, timestamp: new Date('2024-07-18T12:00:00Z') },
+        { transactions_id: 6, userName: 'Ungerer Hattingh', from_account_id: 2, transaction_type: "big", amount: 2500, timestamp: new Date('2024-07-15T12:00:00Z') },
+        { transactions_id: 7, userName: 'Ungerer Hattingh', from_account_id: 2, transaction_type: "small", amount: 3000, timestamp: new Date('2024-08-01T09:00:00Z') },
+        { transactions_id: 8, userName: 'Test for date sorting', from_account_id: 2, transaction_type: "medium", amount: 500, timestamp: new Date('2024-08-21T15:00:00Z') },
+        { transactions_id: 9, userName: 'Ungerer Hattingh', from_account_id: 2, transaction_type: "medium", amount: 5000000, timestamp: new Date('2024-08-22T15:00:00Z') },
+        { transactions_id: 10, userName: 'Ungerer Hattingh', from_account_id: 2, transaction_type: "medium", amount: 54500, timestamp: new Date('2024-08-23T15:00:00Z') },
+        { transactions_id: 11, userName: 'Ungerer Hattingh', from_account_id: 2, transaction_type: "medium", amount: 67000, timestamp: new Date('2024-08-24T15:00:00Z') },
+        { transactions_id: 12, userName: 'Test for date sorting', from_account_id: 2, transaction_type: "medium", amount: 500, timestamp: new Date('2024-08-25T16:40:00Z') },
+        { transactions_id: 13, userName: 'Test for date sorting', from_account_id: 2, transaction_type: "medium", amount: 500, timestamp: new Date('2024-08-25T16:45:00Z') },
+        { transactions_id: 14, userName: 'Test for date sorting', from_account_id: 4, transaction_type: "medium", amount: 500, timestamp: new Date('2024-08-25T16:45:00Z') },
+        { transactions_id: 15, userName: 'Test for date sorting', from_account_id: 4, transaction_type: "medium", amount: 500, timestamp: new Date('2024-08-27T16:45:00Z') },
+        { transactions_id: 16, userName: 'Test for date sorting', from_account_id: 2, transaction_type: "medium", amount: 500, timestamp: new Date('2024-08-26T14:45:00Z') },
     ];
     // to change months from numbers to names
     // console.table(incomingData)
@@ -86,7 +87,12 @@ function Transactions() {
 
     // * useStates ....................................................................................
     const [userTransactions, setUserTransactions] = useState<Transaction[]>([]); // the usestate
-    const [filterOption, setFilterOption] = useState<FilterOption>('ALL');
+    const [filterOption, setFilterOption] = useState<FilterOption>('ALL'); // filter useState
+    const [sortOptions, setSortOptions] = useState<{ primary: 'timestamp' | 'amount' | 'transactions_id', secondary?: 'timestamp' | 'amount' | 'transactions_id' }>({
+        primary: 'timestamp',
+        secondary: 'amount'
+    });
+    // const [sortMethod, setSortMethod] = useState<'timestamp' | 'amount'>('timestamp');
     const [users, setUsers] = useState<User[]>([]);
 
     // * GET all Transactions for the current logged user .........................................
@@ -107,7 +113,7 @@ function Transactions() {
     }, []);
     // console.log(userTransactions);
 
-    
+
 
     // * FILTERING FUNCTOINALITY ..................................................................
 
@@ -128,45 +134,41 @@ function Transactions() {
                 return incomingData;
         }
     })();
-    // console.log(filteredData.length);
+    console.log(filteredData.length);
 
 
     // * SORTING FUNCTOINALITY ....................................................................
-    // hove the option set a value and sort triggered with the value
-    // * Defualt sorting (by timestamp):
-    function defualtSortingByDate(data: Transaction[]): Transaction[] {
-        return data.sort((a, b) => b.timestamp.getTime() - a.timestamp.getTime());
-    }
+    // 
+    const sortData = (data: Transaction[], primary: 'timestamp' | 'amount' | 'transactions_id', secondary?: 'timestamp' | 'amount' | 'transactions_id') => {
+        return [...data].sort((b, a) => {
+            let comparison = 0;
+            // Primary sorting
+            // Determines the initial order of items based on the selection.
+            if (primary === 'timestamp') {
+                comparison = a.timestamp.getTime() - b.timestamp.getTime();
+            } else if (primary === 'amount') {
+                comparison = a.amount - b.amount;
+            } else if (primary === 'transactions_id') {
+                comparison = a.transactions_id - b.transactions_id;
+            }
+            // Secondary sorting
+            // If two items are equal based on the primary criterion, this further sorts them based on the secondary criterion.
+            if (comparison === 0 && secondary) {
+                if (secondary === 'timestamp') {
+                    comparison = a.timestamp.getTime() - b.timestamp.getTime();
+                } else if (secondary === 'amount') {
+                    comparison = a.amount - b.amount;
+                } else if (secondary === 'transactions_id') {
+                    comparison = a.transactions_id - b.transactions_id;
+                }
+            }
 
-    // * Sorting by the Amount:
-    // ? how to execute this...
-    function sortingByAmount(data: Transaction[]): Transaction[] {
-        return data.sort((a, b) => b.amount - a.amount); // sorting by the amount (decending = biggest to smallest)
-    }
-
-    // * this the what results in the sorting of data that is rendered 
-    const sortedData = defualtSortingByDate(incomingData);
-    // const resultDataOfAmount = sortingByAmount(incomingData);
-    // console.log(sortedData);
-
-
-    // TODO : sorting functionality Testing -----
-    // soring options like : by name - by amount - by date - by status 
-    const filterAndSortTransactions = (items: Transaction[]): Transaction[] => {
-        const now = new Date();
-
-        // Filter transactions from the past 24 hours
-        const recentTransactions = items.filter(item => {
-            const itemDate = new Date(item.timestamp); // TODO : Timestamps need to be sorted into date and
-            return itemDate;
+            return comparison;
         });
-
-        // Sort transactions by amount in descending order
-        return recentTransactions.sort((a, b) => b.amount - a.amount);
-
-        // Sort the data by timestamp in ascending order
-        const sortedData = [...incomingData].sort((a, b) => a.timestamp.getTime() - b.timestamp.getTime()); // useless for now as the defualt sorting does this already
     };
+    // Combining the filtering functioniality and the sorting functionality into one filtered variable
+    // by taking the filteredData, both soted options and puting it all together
+    const sortedAndFilteredData = sortData(filteredData, sortOptions.primary, sortOptions.secondary);
 
 
     // * OTHER ....................................................................................
@@ -236,9 +238,9 @@ function Transactions() {
                                 Filter
                             </button>
                             <div className={styles.dropDownContent}>
-                                <button value={"timestamp"}>By Date </button>
-                                <button value={"byName"}>By Name</button>
-                                <button value={"amount"}>By Amount</button>
+                                <button onClick={() => setSortOptions({ primary: 'timestamp', secondary: 'amount' })}>By Date </button>
+                                <button onClick={() => setSortOptions({ primary: 'transactions_id', secondary: 'amount' })}>By ID</button>
+                                <button onClick={() => setSortOptions({ primary: 'amount', secondary: 'timestamp' })}>By Amount</button>
                                 <button value={""}>By Invoice</button>
                                 <button value={"byStatus"}>By Status</button>
                             </div>
@@ -268,43 +270,7 @@ function Transactions() {
                     {/* Content */}
                     <div className={styles.innerTableCon}>
                         {/* // * row of user data */}
-                        <div className={styles.contentRowTile}>
-                            {/* name data block */}
-                            <div className={styles.nameDataBlock}>
-                                <div className={styles.userProfilePlacholder}></div>
-                                {/* User name data and number data block */}
-                                <div className={styles.userNameData}>
-                                    <p className={styles.tableText01}>Name Surname{ }</p>
-                                    <p className={styles.dataIdNumber}>302442</p>
-                                </div>
-                            </div>
-                            {/* Date data block */}
-                            <div className={styles.dateDataBlock}>
-                                <p className={styles.tableText01}>11 july 2024</p>
-                                <p className={styles.timeData}>At 11:00 pm</p>
-                            </div>
-                            {/* Amount Data Block */}
-                            <div className={styles.amountDataBlock}>
-                                <p className={styles.tableText01}>R850.99</p>
-                            </div>
-                            {/* invoice Id Data Block */}
-                            <div className={styles.invoiceDataBlock}>
-                                <p>INV34598</p>
-                            </div>
-                            {/* states data block */}
-                            <div className={styles.statusDataBlock}>
-                                <p className={styles.positiveText}>Recieved</p>
-                            </div>
-                            {/* Action data block */}
-                            <div className={styles.actionDataBlock}>
-                                <button className={styles.detailsBtn} value="Get Models">Details
-                                    <img src={VerticalDots} alt="vertical" />
-                                </button>
-                            </div>
-                        </div>
-                        {/* <li key={Transactions.id}>
-                            {transaction.name}
-                        </li> */}
+                       
                         {/* row of user data */}
                         {/* map testing */}
                         {userTransactions && userTransactions.length > 0 ? (
@@ -347,13 +313,12 @@ function Transactions() {
                             <p>No transactions Found</p>
                         )}
 
-                        <h3>Testing Dummy data below:</h3>
-                        <p>Defualt Sorting is by Date </p>
+                        <p>Testing Dummy data below:</p>
 
                         {/* // * MAPPING FROM DUMMY DATA -------------------------------------------------------------- */}
-                        {filteredData && filteredData.length > 0 ? (
-                            filteredData.map(transaction => (
-                                <div className={styles.contentRowTile}>
+                        {sortedAndFilteredData && sortedAndFilteredData.length > 0 ? (
+                            sortedAndFilteredData.map(transaction => (
+                                <div key={transaction.transactions_id} className={styles.contentRowTile}>
                                     {/* name data block */}
                                     <div className={styles.nameDataBlock}>
                                         <div className={styles.userProfilePlacholder}></div>
